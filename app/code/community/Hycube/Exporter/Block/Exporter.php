d4d8cde7fe680d5f406cf925cc2a70a2b5aa0585
badi238 <obaid.cgf@gmail.com>
2018-05-25 12:35:14 +0500

    add code

---
+++ app/code/community/Hycube/Exporter/Block/Exporter.php
@@ -0,0 +1,20 @@
+<?php
+
+class Hycube_Exporter_Block_Exporter extends Mage_Core_Block_Template
+{
+
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+
+    public function getExporter()
+    {
+        if (!$this->hasData('exporter')) {
+            $this->setData('exporter', Mage::registry('exporter'));
+        }
+
+        return $this->getData('exporter');
+    }
+
+}
